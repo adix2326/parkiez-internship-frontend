c2783215ac2558e3559a6d1ce64d233d230dd8c1
Aditya Dhamale <aditya1dhamale1@gmail.com>
2024-06-13 12:23:05 +0530

    first commit

---
+++ parkiez-frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import Button from './components/button'
+
+export default function App() {
+  return (
+    <h1 className="">
+      <Button/>
+    </h1>
+  )
+}
---
+++ parkiez-frontend/src/components/button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const button = () => {
+  return (
+    <>
+    
+    <div>Hello Shreya !!</div>
+    
+    </>
+  )
+}
+
+export default button
